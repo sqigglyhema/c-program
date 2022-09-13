06c506113dc91efd46b1db279ce9e20178059139
sqigglyhema <113290948+sqigglyhema@users.noreply.github.com>
2022-09-13 11:24:29 +0530

    Add files via upload

---
+++ ascending and desending.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int main  ascending and descending order;
+	printf("Enter the alphabets:");
+	scanf("%d,"order alphabets:);
+	}
+	{
+	printf("apple,banana,radish,jack,carrot:");
+	return 0;
+}
+	
+
